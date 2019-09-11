d0c6050e647da4854528ff13063b7242961c39b9
annakuzniak <aniaquzniak@gmail.com>
2019-09-11 20:12:39 +0200

    Updates

---
+++ precache-manifest.534e9b055edf6da343bca083372a9df7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5210c574ec007f99ef9fcabea34cf4a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "870e547432b9167f63db",
+    "url": "/monsters-rolodex/static/css/main.30482fb3.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "870e547432b9167f63db",
+    "url": "/monsters-rolodex/static/js/main.f32f6313.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
